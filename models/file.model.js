5e5e0f02d5e0ad64fea50093dba62df0d56b9f63
Tushar Ranjan <thrrnjn1103@gmail.com>
2024-12-08 13:27:16 +0530

    added mail functionality using nodemailer

--- models/file.model.js
+++ models/file.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import nodemailer from "nodemailer"
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,5 +16,35 @@ const fileSchema = new mongoose.Schema({
         type: String,
     },
 }, {timestamps: true});
+
+fileSchema.post("save", async function (doc) {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: process.env.MAIL_USER,
+              pass: process.env.MAIL_PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: 'tushar',
+            to: doc.email,
+            subject: 'Mail from Server ',
+            html: `<h2>Hello </h2><p>File is uploaded</p><p>Image: <a href="${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+            attachments: [
+                {
+                    filename: `image-1.jpeg`,
+                    path: "public/temp/PHOTO Medium Small.jpeg"
+                }
+            ]
+        }
+
+        const mailResponse = await transporter.sendMail(mailOptions);
+        console.log("MAIL Sent : ", mailResponse.response)
+    } catch (error) {
+        console.error("Error sending Mail")
+    }
+})
 
 export const File = mongoose.model('File', fileSchema);
